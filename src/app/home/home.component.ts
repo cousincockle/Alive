ff854ce2a5bf4eed7ba95c651a5ad5e879d62554
Anouar Mabrouk <mabrouk@ascora.de>
2018-11-17 05:25:21 +0100

    add ng-boostrap + remove popper jquery

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { APIModel } from 'src/models/APIModel';
+import { statusModel } from 'src/models/statusModel';
 const {ipcRenderer} = (<any>window).require("electron");
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -8,8 +11,17 @@ const {ipcRenderer} = (<any>window).require("electron");
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  APIs: APIModel[] = []
+  testAPI : APIModel
+  test2 : APIModel
+  url : string = 'https://mediadl-api.abelssoft.de/v1/gettop10?access_token=C2466CAA4876AD44B353669D65B537D25DB21613F7534AADF16E98DD328D826D&type=documentation'
+  constructor() {
+    this.testAPI = new APIModel('Mediathek',statusModel.alive,this.url,20,'/Users/Anouar/Documents/Sources/2018/MediaDownloader_CrossPlatform/src/assets/app-icon/icon-win.png')
+    this.test2 = new APIModel('Xparser',statusModel.alive,this.url,20,'/Users/Anouar/Documents/Sources/2018/MediaDownloader_CrossPlatform/src/assets/app-icon/icon-win.png')
 
-  constructor() { }
+    this.APIs.push(this.testAPI,this.test2)
+    console.dir(this.APIs)
+   }
 
   ngOnInit() {
   } 
